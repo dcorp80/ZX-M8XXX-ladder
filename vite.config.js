52d9731ecbcb90bb40b95998260ef82a239c492d
dcorp80 <10655954+dcorp80@users.noreply.github.com>
2026-02-28 21:23:48 +0100

    Init npm, move paco to depts, add vite

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+    base: './',
+    define: {
+        APP_VERSION: JSON.stringify('0.9.31'),
+    },
+    resolve: {
+        extensions: ['.mts', '.ts', '.tsx', '.mjs', '.js', '.jsx', '.json'],
+    },
+    build: {
+        chunkSizeWarningLimit: 600,
+    },
+})
